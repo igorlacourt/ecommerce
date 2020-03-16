c4d0ede97396c9f496fc102e45d5902124a5fb64
Igor Lacourt <igorlacourt@gmail.com>
2020-03-16 09:36:30 -0300

    Update firebase.js
    Sensitive information removed.

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
   import firebase from 'firebase';
   // Your web app's Firebase configuration
   var config = {
-    apiKey: "AIzaSyAOsR-qdwzQL41VE2hnF9WePtlc9s_yw4s",
-    authDomain: "global-chat-7dcd9.firebaseapp.com",
-    databaseURL: "https://global-chat-7dcd9.firebaseio.com",
-    projectId: "global-chat-7dcd9",
-    storageBucket: "global-chat-7dcd9.appspot.com",
-    messagingSenderId: "488958275718",
-    appId: "1:488958275718:web:ef2c37c1a15864156ce107"
+    // Sensitive information removed
   };
   // Initialize Firebase
   export const fb = firebase.initializeApp(config);
